7811c32169292439c0d033ad5be4348311025094
Geoff Kimball <geoff@zurb.com>
2015-09-03 15:22:54 -0700

    Add JavaScript plugin for animating elements in and out

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@ var sass = require('gulp-sass');
 var rubySass = require('gulp-ruby-sass');
 var rename = require('gulp-rename');
 var Super = require('supercollider').init;
+var umd = require('gulp-umd');
 
 gulp.task('docs', function() {
   return gulp.src('./docs/*.md')
@@ -21,17 +22,40 @@ gulp.task('sass', function() {
     .pipe(gulp.dest('./build/assets'));
 });
 
-gulp.task('dist', ['sass'], function() {
-  return gulp.src('./build/assets/motion-ui.css')
+gulp.task('javascript', function() {
+  return gulp.src('./motion-ui.js')
+    .pipe(umd({
+      dependencies: function(file) {
+        return [{ name: 'jquery', amd: 'jquery', cjs: 'jquery', global: 'jQuery', param: '$' }];
+      },
+      exports: function(file) {
+        return 'MotionUI';
+      },
+      namespace: function(file) {
+        return 'MotionUI';
+      }
+    }))
+    .pipe(gulp.dest('./build/assets'));
+});
+
+gulp.task('dist', ['sass', 'javascript'], function() {
+  gulp.src('./build/assets/motion-ui.css')
     .pipe(gulp.dest('./dist'))
     .pipe(sass({
       outputStyle: 'compressed'
     }))
     .pipe(rename('motion-ui.min.css'))
     .pipe(gulp.dest('./dist'));
+
+  return gulp.src('./build/assets/motion-ui.js')
+    .pipe(gulp.dest('./dist'))
+    .pipe(uglify())
+    .pipe(rename('motion-ui.min.js'))
+    .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('default', ['docs', 'sass'], function() {
+gulp.task('default', ['docs', 'sass', 'javascript'], function() {
   gulp.watch('./docs/*.md', ['docs']);
   gulp.watch(['./src/**/*.scss', './motion-ui.scss'], ['sass']);
+  gulp.watch('./motion-ui.js', ['javascript']);
 });
